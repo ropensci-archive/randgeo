78c387864370aa75f5d2ab09642766b33a8e8c2a
Scott Chamberlain <myrmecocystus@gmail.com>
2017-02-21 09:16:56 -0800

    add @noamross to authors, bump patch version and add dev ver updated pkg level man file with author and add notes about package API

--- R/randgeo-package.R
+++ R/randgeo-package.R
@@ -1,11 +1,21 @@
 #' Random WKT or GeoJSON
 #'
-#' adapted from \url{https://github.com/tmcw/geojson-random} to have a pure
+#' Adapted from \url{https://github.com/tmcw/geojson-random} for a pure
 #' R implementation
+#'
+#' @section Package API:
+#' \itemize{
+#'  \item rg_position - random position
+#'  \item geo_point - random GeoJSON point
+#'  \item geo_polygon - random GeoJSON polygon
+#'  \item wkt_point - random WKT point
+#'  \item wkt_polygon - random WKT polygon
+#' }
 #'
 #' @name randgeo-package
 #' @aliases randgeo
 #' @docType package
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @author Noam Ross
 #' @keywords package
 NULL
